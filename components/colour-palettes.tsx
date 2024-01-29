7bc62ece395b7a0928ba8bdff1897a98aed8d943
kcirreb <koberrick11@gmail.com>
2024-01-29 16:10:23 +0800

    fix fetch error

--- components/colour-palettes.tsx
+++ components/colour-palettes.tsx
@@ -44,6 +44,7 @@ export default function ColourPalettes() {
     try {
       const response = await fetch("http://colormind.io/api/", {
         method: "POST",
+        mode: "no-cors",
         body: JSON.stringify({ model: "ui" }),
       });
       const data = await response.json();
