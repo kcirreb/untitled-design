f3969b5d6042f8bbe850a0cef2d267fe090c8b5b
kcirreb <koberrick11@gmail.com>
2025-01-09 16:16:36 +0800

    style: limit width of page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Demo from "@/components/demo";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-12 gap-10">
+    <main className="flex flex-col items-center justify-between p-12 gap-10 max-w-screen-2xl mx-auto">
       <Header />
       <div className="flex flex-col gap-4 w-full">
         <div className="text-3xl font-bold">Editor</div>
